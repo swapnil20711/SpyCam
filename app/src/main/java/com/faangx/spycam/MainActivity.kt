9e8a01b73839a2c7821550f2855274ca5d72a016
swapnil7000 <swapnil.bhojwani789@gmail.com>
2022-02-25 18:02:44 +0530

    Initial commit

---
+++ app/src/main/java/com/faangx/spycam/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.faangx.spycam
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
